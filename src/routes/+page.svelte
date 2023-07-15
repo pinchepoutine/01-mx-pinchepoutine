70bd5e7cf9942a8048cccf6c53c830f366e96379
Chef <cheftech@patraldo.dev>
2023-07-15 11:54:05 -0600

    wtf's happening with my keyboard?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
 
 <h2>Poutinería ¡Pinche Poutine! QEPD</h2>
 
-<p>Gracias infinitamente a tod@s nuestr@s querid@s clientes.  Fue un placer abrir nuestras puertas en 2016 por primera vez y recibir tanta gente de buena onda, curiosa, y divertida.  Cerramos permanentemente el restaurante al final de junio de 2023.  Seguiremos con nuestro negocio de plantas en el mismo lugar de ¡Pinche Poutine!  Antoine seguirá con sus dibujos y Christophe R con su código abierto. Si dios quiere, l@s invitaremos a talleres de arte y <a href="https://antoine.patraldo.com/talleres"> de animación.</a></p>
+<p>Gracias infinitamente a tod@s nuestr@s querid@s clientes.  Fue un placer abrir nuestras puertas en 2016 por primera vez y recibir tanta gente de buena onda, curiosa, y divertida.  Cerramos permanentemente el restaurante al final de junio de 2023.  Seguiremos con nuestro negocio de plantas en el mismo lugar de ¡Pinche Poutine!  Antoine seguirá con sus dibujos y Christophe R con su código abierto. Si dios quiere, pronto l@s invitaremos a talleres de arte y <a href="https://antoine.patraldo.com/talleres"> de animación.</a></p>
