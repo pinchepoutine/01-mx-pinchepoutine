cd5575ccc80a27bc734d931d217f9536ebb34a4f
Chef <cheftech@patraldo.dev>
2023-07-22 11:32:39 -0600

    add more links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <h1><a href="https://plantas.patraldo.com">Viva ¡Plantas Patraldo!</a></h1>
 
+<h1><a href="https://antoine.patraldo.com">Viva ¡Antoine Patraldo!</a></h1>
+
+<h1><a href="https://antoine.patraldo.com/exhibiciones">Viva ¡La Musa que Mas Aplauda!</a></h1>
+
+<h1><a href="https://pinchepoutine.digital">Viva ¡Pinche Poutine! Digital</a></h1>
+
 <h2>Poutinería ¡Pinche Poutine! QEPD</h2>
 
-<p>Gracias infinitamente a tod@s nuestr@s querid@s clientes.  Fue un placer abrir nuestras puertas en 2016 por primera vez y recibir tanta gente de buena onda, curiosa, y divertida.  Cerramos permanentemente el restaurante al final de junio de 2023.  Seguiremos con nuestro negocio de plantas en el mismo lugar de ¡Pinche Poutine!  Antoine seguirá con sus dibujos y Christophe R con su código abierto. Si dios quiere, pronto l@s invitaremos a talleres de arte y <a href="https://antoine.patraldo.com/talleres"> de animación.</a></p>
+<p>Gracias infinitamente a tod@s nuestr@s querid@s clientes.  Fue un placer abrir nuestras puertas en 2016 por primera vez y recibir tanta gente de buena onda, curiosa, y divertida.  Cerramos permanentemente el restaurante al final de junio de 2023.  Seguiremos con nuestro negocio de plantas en el mismo lugar de ¡Pinche Poutine!  Antoine seguirá con sus dibujos y Christophe R con su código abierto. Si dios quiere, pronto l@s invitaremos a exhibiciones, talleres de arte y <a href="https://antoine.patraldo.com/talleres"> de animación.</a></p>
